8acc0bda2e149d7f5e6152037a2d9c02f637cd78
KENKUN-1031 <kencoding1031@gmail.com>
2020-12-26 14:34:08 +0900

    yay2

--- js/history.js
+++ js/history.js
@@ -1,17 +1,8 @@
-
+import line1 from "~/lines/html/line1.html"
 
 export default {
   name: "Index",
   components: {line1, },
-
-  head() {
-    return {
-      title: "歴史-高校生による高校生のため黒人差別",
-      meta: [
-        { hid: "description", name: "description", content: "米国の歴史、また黒人差別の歴史を表記しています。" }
-      ]
-    }
-  },
 
   data(){
     return{
