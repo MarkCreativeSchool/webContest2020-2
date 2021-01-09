39768c7967b5c03463315993d66f90cfe535069a
KENKUN-1031 <kencoding1031@gmail.com>
2021-01-09 16:32:41 +0900

    timeline

--- js/history/en/linebar.js
+++ js/history/en/linebar.js
@@ -1,9 +1,22 @@
 var lineBar = Vue.component("lineBar", {
     name: "lineBar",
     template: `
-<p class="lineBar" :style="{ width: width + 'px', left: left + 'px'}" @click="onClick()">
-    <slot></slot>
-</p>`,
+<!--<p class="lineBar" :style="{ width: width + 'px', left: left + 'px'}" @click="onClick()">-->
+<!--    <slot></slot>-->
+<!--</p>-->
+
+<div class="eventLine" :style="{width:width + 'px', left: left + 'px'}" @click="onClick()">
+    <div class="linePoint"></div>
+    <div class="lineBorder">
+        <div class="borderLine1"></div>
+        <div class="borderLine2"></div>
+    </div>
+    <div class="lineBalloon"><slot></slot></div>
+</div>
+
+`,
+
+
     props:["width", "left", "number"],
 
     methods:{
