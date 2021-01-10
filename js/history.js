2e0fe221c6b41f121d2fcf5892e0bcf99db057a2
KENKUN-1031 <kencoding1031@gmail.com>
2021-01-10 12:31:01 +0900

    timeline

--- js/history.js
+++ js/history.js
@@ -20,9 +20,14 @@ var app = new Vue({
       this.chapter = section
       this.coreData = data
       this.isOpen = true;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${window.scrollY}px`;
     },
     close(){
       this.isOpen = false;
+      document.body.style.position = '';
+      document.body.style.top = '';
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
     }
   }
 })
