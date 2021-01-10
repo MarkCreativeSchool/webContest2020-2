7460bae6f592f2cc1433872e55dadbb7de5f9fd2
matoi0317 <matoi.shibanuma@gmail.com>
2021-01-10 12:13:56 +0900

    eng map

---
+++ js/map/en/data.js
@@ -0,0 +1,146 @@
+var mapData = {
+    "ny":{
+        articles: [
+            {
+                title: "Shooting of Sean Bell",
+                subtitle: "New York",
+                year:"2006",
+                text: "On November 25, 2006, a man named Sean Bell (23 years old), was shot by the police a total of 50 times. He was shot after being seen leaving a drug-plagued strip joint with his friends. The police shot due to the fact that they thought they would be armed, for the place itself was full of illegal actions. Although Sean Bell was shot 50 times as well as his friends were critically injured, the police were fired, but ended in being not guilty. "
+            },
+            {
+                title: "Killing of Eric Garner",
+                subtitle: "New York",
+                year:"2014",
+                text: "In 2014, July 17, Eric Garner, was seized by the police after he was found to be illegally selling loose cigarettes. However, the officer did not end there, and instead, held Garner down in a chokehold, that led to his death. During this time, Garner repeated the phrase “I can’t breathe” 11 times, which is now being brought up once again with George Floyd’s death. Although the officer, Daniel Pantaleo was fired, he was acquitted from this crime."   
+            }
+        ]
+    },
+
+    "or":{
+        articles: [
+            {
+                title: "Tear gas",
+                subtitle: "Oregon",
+                year:"2020",
+                text: "On June 26th, the protesters pulled down the barricades, and caused a fire in the police buildings. In response, the police used tear gas in order to disperse the protesters. The mayor later on banned the use of CR Tear gas in portland. Furthermore, the police made a decision to increase the level of security in Portland."
+            }
+        ]
+    },
+    "ca":{
+        articles: [
+            {
+                title: "Shooting of Oscar Grant",
+                subtitle: "California",
+                year:"2009",
+                text: "On January 1, 2009, Oscar Grant, a 22 year old man, was murdered at a subway station. He was detained along with approximately 20 passengers that were involved in a fight in the train. Although he was not involved in the fight, he was near the location the fight took place and was captured. While trying to confront the officer, he was backed against the wall and was handcuffed, and later murdered."
+            }
+        ]
+    },
+    "mn":{
+        articles: [
+            {
+                title: "Philando Castile death",
+                subtitle: "Minnesota",
+                year:"2006",
+                text: "On July 6, 2016, Philando Castile was shot by the police after being stopped by the police. Castile was driving the car while he was stopped by the police due to his backlight being off. He followed the orders and told the police that he was armed, and was trained to do so in case the police attacked him. He then turned around to show his driver’s license in which the police officer mistakenly thought he was going to get out his gun and ultimately shot him."
+            }
+        ]
+    },
+    "mo":{
+        articles: [
+            {
+                title: "Shooting of Michael Brown",
+                subtitle: "Missouri",
+                year:"2014年",
+                text: "Shortly after noon, Michael Brown was on his way home from a convenience store with a friend when he got into an argument with one police officer. Brown fiercely resisted and attempted to take the gun, and a struggle ensued, and when the officer ordered him to restrain him, he lunged at the officer, and the police opened fire, killing Brown at the scene. The police officer was not charged."
+            }
+        ]
+    },
+    "il":{
+        articles: [
+            {
+                title: "Demo Attack in Mayor’s House",
+                subtitle: "Illinois",
+                year:"2020",
+                text: "In Chicago, protesters gathered around the mayor’s house. During this protest, the protesters threw rocks, fireworks, frozen bottles, and other objects to attack officers, which led to 18 officers getting injured. 12 protesters were arrested from this demo. The Civilian Office of Police Accountability (COPA) confirmed that they are currently investigating the numerous complaints that have been sent regarding police misconduct. With this incident, it has been reported that the security level of Chicago increased."
+            }
+        ]
+    },
+
+    "ky":{
+        articles: [
+            {
+                title: "Shooting of Breonna Taylor",
+                subtitle: "Kentucky",
+                year:"2020",
+                text: "On March 13, 2020, Breonna Taylor (26 years old) was fatally shot by the police after the police intruded her and her boyfriend’s house after being accused of drug possession. Breonna’s boyfriend thought this act was trespassing, and shot the police, and in response, the police shot Breonna. The trial ended with Breonna’s boyfriend (Kenneth Walker) being pressed charges of shooting, but the officer was acquitted from his crime."
+            }
+        ]
+    },
+    "fl":{
+        articles: [
+            {
+                title: "Shooting of Trayvon Martin",
+                subtitle: "Florida",
+                year:"2012",
+                text: "On February 26, 2012, an African-American boy who was 17 year old at the time was shot by a Hispanic neighborhood watch captain because he thought he was “suspicious.” However, this lawsuit ended with the watch captain (George Zimmerman) being declared as “not guilty/innocent”. This event started the “Blacks lives Matter” movement, which will be explained more in the organizations page."
+            }
+        ]
+    },
+    "ga":{
+        articles: [
+            {
+                title: "Shooting of Ahmaud Arbery",
+                subtitle: "Georgia",
+                year:"2020",
+                text: "Ahmaud Arbery (24 years old) was murdered by Gregory McMichael (64 years old) and Travis McMichael (34 years old) on February 23, 2020. According to the police, Gregory and Travis (parents and child) thought that Arbery had caused the robbery that occurred near their house, and was running away. With no evidence, they fatally shot Arbery. They were acquitted from their crime, which caused protests amongst the citizens."
+            }
+        ]
+    },
+    "md":{
+        articles: [
+            {
+                title: "Death of Freddie Gray",
+                subtitle: "Maryland",
+                year:"2015",
+                text: "In 2015, April 12, Freddie Gray was arrested due to possessing a knife. He was captured by 6 police officers, who held him to the police van while he was protesting. This caused injuries within the spinal cord, and caused Gray to slip into a coma. 3 days after the arrest, Freddie Gray was declared dead. Although the officers were pressed charges for conducted violence and death, the result of the trial ended with the officers declared as “not guilty”."
+            }
+        ]
+    },
+    "oh":{
+        articles: [
+            {
+                title: "Shooting of John Crawford III",
+                subtitle: "Ohio",
+                year:"2015",
+                text: "In 2013, August 5, John Crawford III (22 years old) was shot by the police after attempting to steal at Walmart. The police officer,  Sean Williams was pressed with no charges."
+            },
+            {
+                title: "Shooting of Tamir Rice",
+                subtitle: "Ohio",
+                year:"2014",
+                text: "On November 22, 2014, a 12 year old boy named Tamir Rice was shot after he was mistakenly perceived by the police to have been holding a gun (in reality, it was a toy gun). The 3 officers have been pressed charges and fired, while the fourth office is still being investigated."
+            }
+        ]
+    },
+    "pa":{
+        articles: [
+            {
+                title: "Shooting of Antwon Rose Jr.",
+                subtitle: "Pennsylvania",
+                year:"2018",
+                text: "In 2018, June 19, Antwon Rose Jr. (17 years old) was shot after trying to escape from a false accusation made by an office (*needs source). He was riding in the passenger seat of the car that was apparently seen near a shooting that had just occurred that day, and was stopped by the police. The officer shot him 3 times and Antwon was declared dead. The officer was acquitted for all charges."
+            }
+        ]
+    },
+    "ma":{
+        articles: [
+            {
+                title: "Racial Justice Rally in Boston",
+                subtitle: "Massachusetts",
+                year:"2020",
+                text: "On September 5, 2020, several “Black Lives Matter” supporters gathered to conduct a racial justice rally In Boston, Massachusetts. The supporters gathered in Copley Square, where the held photos of victims who were killed by shootings. Furthermore, students gathered and called for investment for black communities and defunding the police. "
+            }
+        ]
+    },
+}
---
+++ js/map/en/modal.js
@@ -0,0 +1,37 @@
+var modal = Vue.component("modal", {
+    name: "modal",
+    template: `
+    <div class="modal is-active" v-if="isOpen === true">
+  <div class="modal-background" @click="close"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <!-- <button class="delete" aria-label="close"></button> -->
+    </header>
+    <section class="modal-card-body">
+        {{ mapFileData }}
+        <!--
+        <img :src="'./img/' + mapFileData" />
+        -->
+        <ul>
+            <li v-for="article in mapArticles">
+              <div class="article-title-wrapper">
+                <div class="article_title">●{{ article.title }}</div>
+                <div class="article_subtitle">({{ article.year }})</div>
+              </div>
+              <p>{{ article.text }}</p>
+            </li>
+        </ul>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button" @click="close">Close</button>
+    </footer>
+  </div>
+</div>
+    `,
+    props: ["isOpen", "mapFileData", "mapArticles"],
+    methods: {
+        close(){
+            this.$emit("close")
+        }
+    }
+})
